16700e8c4b3fcfbe764da7648dbb55b8051d34b6
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 16:27:09 -0500

    Created shared scheme of FXBlurView

---
+++ Examples/BasicExample/FXBlurView/FXBlurView.h
@@ -0,0 +1,19 @@
+//
+//  FXBlurView.h
+//  FXBlurView
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 Charcoal Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FXBlurView.
+FOUNDATION_EXPORT double FXBlurViewVersionNumber;
+
+//! Project version string for FXBlurView.
+FOUNDATION_EXPORT const unsigned char FXBlurViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXBlurView/PublicHeader.h>
+
+
